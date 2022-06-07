a0273afbe55b7ce0495191ab40681958ec883293
Seth <seth7293@gmail.com>
2022-06-07 14:07:17 -0600

    6-7 auth0 setup

---
+++ client/app/env.js
@@ -0,0 +1,8 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-9ptc0fv8.us.auth0.com'
+export const audience = 'https://SethDev.com'
+export const clientId = 'TWUCpgDir5QlmcwhsdBwGZOUMItsxpMD'
+
+
+
